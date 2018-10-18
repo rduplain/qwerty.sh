e1e1ee3243f291840ef3c614dbf50fcbbb6416e2
Ron DuPlain <ron.duplain@gmail.com>
2018-10-18 02:23:32 -0400

    Download .Makefile.d only once.

--- web/.Makefile.d-init.mk
+++ web/.Makefile.d-init.mk
@@ -1,19 +1,18 @@
-# Set DIR to absolute path of directory containing this .mk file.
+__FILE__ := $(abspath $(lastword $(MAKEFILE_LIST)))
 DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 
-include .Makefile.d/path.mk
-
 define qwerty-Makefile.d
-cat $(DIR)/../qwerty.sh |\
+cat $(abspath $(DIR)/../qwerty.sh) |\
 	sh -s - \
 	--sha256=0a7a636fa9c6d4838d2837884d66fd26f2cf910df9e62b6a24821f9235c7cf86 \
 	https://github.com/rduplain/Makefile.d/tarball/04f0ab7 |\
 		tar -xvzf - --strip-components=1
 endef
 
-.Makefile.d/%.mk:
+.Makefile.d/%.mk: .Makefile.d/path.mk
+	@touch $@
+
+.Makefile.d/path.mk:
 	@mkdir -p .Makefile.d
 	cd .Makefile.d; $(qwerty-Makefile.d)
-
-prove-Makefile.d:
-	@echo $(PROJECT_ROOT)
+	@touch $@
