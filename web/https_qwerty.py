076ed196b5931331563e97f3b34ef480fe71c606
Ron DuPlain <ron.duplain@gmail.com>
2018-10-18 00:48:02 -0400

    Serve qwerty.sh directly, without GitHub.
    qwerty.sh is already a single-point of failure. While GitHub is stable,
    requiring an additional service creates another point of failure.

--- web/https_qwerty.py
+++ web/https_qwerty.py
@@ -1,50 +1,107 @@
 """https_qwerty.py: serve qwerty.sh file as requested."""
 
+import subprocess
+
 from wsgi_qwerty import string_response
 
 
-SHELL_REDIRECT = """
+SHELL_NOT_FOUND = """
 #!/usr/bin/env sh
-echo 'error: Use `-L` to support redirects.'  >&2
-echo                                          >&2
-echo 'curl -sSL qwerty.sh'                    >&2
+echo "qwerty.sh: revision '{}' not found."  >&2
 
 exit 2
 """.strip() + '\n'
+
+
+def application(environ, start_response):
+    """WSGI callable to serve qwerty.sh file as requested.
+
+    URL path indicates git ref to serve, defaulting to current HEAD.
+    Example:
+
+        https://qwerty.sh/c0ffee # => serve qwerty.sh at ref c0ffee
+    """
+    req_ref = parse_ref(environ.get('PATH_INFO', '/'))
+    ref = resolve_ref(req_ref)
+
+    if ref is None:
+        start_response(
+            # HTTP Status
+            '404 NOT FOUND',
+
+            # HTTP Response Headers
+            (('Content-Type', 'text/plain'),))
+
+        return string_response(SHELL_NOT_FOUND.format(req_ref))
+    else:
+        start_response(
+            # HTTP Status
+            '200 OK',
+
+            # HTTP Response Headers
+            (('Content-Type', 'text/plain'),))
+
+        return string_response(git_show(ref, 'qwerty.sh'))
+
+
+def resolve_ref(ref):
+    """Resolve to an exact ref, else None."""
+    try:
+        return git_rev_parse(ref)
+    except CommandFailure:
+        for remote in git_remote():
+            try:
+                return git_rev_parse(f'{remote}/{ref}')
+            except CommandFailure:
+                continue
+        return None
 
 
 def parse_ref(url_path):
     """Parse URL which has a git ref."""
     ref = url_path.lstrip('/')
     if not ref:
-        ref = 'master'
+        ref = 'HEAD'
     return ref
 
 
-def github(ref, username='rduplain', domain='raw.githubusercontent.com'):
-    """Build URL for GitHub location of raw qwerty.sh file matching git ref."""
-    return f'https://{domain}/{username}/qwerty.sh/{ref}/qwerty.sh'
+def git_remote():
+    """List of remote names as returned by `git remote`."""
+    return sh('git', 'remote').strip().split('\n')
 
 
-def application(environ, start_response):
-    """WSGI callable to serve qwerty.sh file as requested.
+def git_rev_parse(ref):
+    """Run `git rev-parse --short` on given ref, return stdout."""
+    return sh('git', 'rev-parse', '--short', ref).strip()
 
-    URL path indicates git ref to serve, defaulting to 'master'.
-    Example:
 
-        https://qwerty.sh/c0ffee # => serve qwerty.sh at ref c0ffee
-    """
-    ref = parse_ref(environ.get('PATH_INFO', '/'))
+def git_show(ref, filepath):
+    """Provide file content at given ref (via `git show`)."""
+    return sh('git', 'show', f'{ref}:{filepath}')
 
-    start_response(
-        # HTTP Status
-        '301 MOVED PERMANENTLY',
 
-        # HTTP Response Headers
-        (('Content-Type', 'text/plain'),
-         ('Location', github(ref))))
+def sh(*args, **kw):
+    """Run shell command, returning stdout. Raise error on non-zero exit."""
+    options = dict(
+        encoding='utf-8',
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+    )
+    options.update(kw)
 
-    return string_response(SHELL_REDIRECT)
+    process = subprocess.run(args, **options)
+
+    if process.returncode != 0:
+        raise CommandFailure(process)
+
+    return process.stdout
+
+
+class CommandFailure(subprocess.SubprocessError):
+    """A command failed."""
+
+    def __init__(self, completed_process):
+        super().__init__(completed_process.stderr)
 
 
 if __name__ == '__main__':
