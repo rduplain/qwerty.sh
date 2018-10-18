d9b43d54d1c92a612a218c6e78ff1c74174c1454
Ron DuPlain <ron.duplain@gmail.com>
2018-10-18 03:10:17 -0400

    Download Makefile.d updates upon init file change.

--- web/.Makefile.d-init.mk
+++ web/.Makefile.d-init.mk
@@ -1,8 +1,16 @@
 __FILE__ := $(abspath $(lastword $(MAKEFILE_LIST)))
 DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 
+# On changes to .Makefile.d-init.mk, MAKEFILE_LIST may have lastword pointing
+# to .Makefile.d/path.mk. To bootstrap using qwerty.sh, find it accordingly.
+ifeq ($(notdir $(DIR)), "Makefile.d")
+QWERTY_SH := $(abspath $(DIR)/../../qwerty.sh)
+else
+QWERTY_SH := $(abspath $(DIR)/../qwerty.sh)
+endif
+
 define qwerty-Makefile.d
-cat $(abspath $(DIR)/../qwerty.sh) |\
+cat $(QWERTY_SH) |\
 	sh -s - \
 	--sha256=0a7a636fa9c6d4838d2837884d66fd26f2cf910df9e62b6a24821f9235c7cf86 \
 	https://github.com/rduplain/Makefile.d/tarball/04f0ab7 |\
@@ -12,7 +20,7 @@ endef
 .Makefile.d/%.mk: .Makefile.d/path.mk
 	@touch $@
 
-.Makefile.d/path.mk:
+.Makefile.d/path.mk: $(__FILE__)
 	@mkdir -p .Makefile.d
 	cd .Makefile.d; $(qwerty-Makefile.d)
 	@touch $@
