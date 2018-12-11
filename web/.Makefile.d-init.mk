da0803338ce278d23e41d62316adf5d35e8b31ee
Ron DuPlain <ron.duplain@gmail.com>
2018-12-10 21:54:01 -0500

    Use latest qwerty.sh features to init Makefile.d.

--- web/.Makefile.d-init.mk
+++ web/.Makefile.d-init.mk
@@ -9,18 +9,13 @@ else
 QWERTY_SH := $(abspath $(DIR)/../qwerty.sh)
 endif
 
-define qwerty-Makefile.d
-cat $(QWERTY_SH) |\
-	sh -s - \
-	--sha256=0a7a636fa9c6d4838d2837884d66fd26f2cf910df9e62b6a24821f9235c7cf86 \
-	https://github.com/rduplain/Makefile.d/tarball/04f0ab7 |\
-		tar -xvzf - --strip-components=1
-endef
+MAKEFILE_D_REPO := https://github.com/rduplain/Makefile.d.git
+MAKEFILE_D_REF := 04f0ab7
 
 .Makefile.d/%.mk: .Makefile.d/path.mk
 	@touch $@
 
 .Makefile.d/path.mk: $(__FILE__)
-	@mkdir -p .Makefile.d
-	cd .Makefile.d; $(qwerty-Makefile.d)
+	cat $(QWERTY_SH) |\
+		sh -s - -f -o .Makefile.d --ref $(MAKEFILE_D_REF) $(MAKEFILE_D_REPO)
 	@touch $@
