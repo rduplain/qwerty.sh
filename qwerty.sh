e496eb61eedb637b29b170a7bf484c80012db48f
Ron DuPlain <ron.duplain@gmail.com>
2018-12-10 22:02:16 -0500

    Mark HEAD as v0.4.3.

--- qwerty.sh
+++ qwerty.sh
@@ -5,7 +5,7 @@
 # README: https://github.com/rduplain/qwerty.sh#readme
 # Contact: community@qwerty.sh -- See footer for BSD 2-Clause License.
 
-VERSION=v0.4.2
+VERSION=v0.4.3-dev
 
 usage() {
     exists "$@" && stderr "$PROG: $(red "$@")" && stderr # Optional message.
