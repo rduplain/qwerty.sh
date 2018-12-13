16562bf963a06e9218e7490b8d6526c2d58fc6c7
Ron DuPlain <ron.duplain@gmail.com>
2018-12-13 00:13:31 -0500

    Use URL and REV as configuration keywords.

--- web/.Makefile.d-init.mk
+++ web/.Makefile.d-init.mk
@@ -9,13 +9,13 @@ else
 QWERTY_SH := $(abspath $(DIR)/../qwerty.sh)
 endif
 
-MAKEFILE_D_REPO := https://github.com/rduplain/Makefile.d.git
-MAKEFILE_D_REF := 04f0ab7
+MAKEFILE_D_URL := https://github.com/rduplain/Makefile.d.git
+MAKEFILE_D_REV := 04f0ab7
 
 .Makefile.d/%.mk: .Makefile.d/path.mk
 	@touch $@
 
 .Makefile.d/path.mk: $(__FILE__)
 	cat $(QWERTY_SH) |\
-		sh -s - -f -o .Makefile.d --ref $(MAKEFILE_D_REF) $(MAKEFILE_D_REPO)
+		sh -s - -f -o .Makefile.d --ref $(MAKEFILE_D_REV) $(MAKEFILE_D_URL)
 	@touch $@
