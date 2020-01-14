add6cfe3a6a229628ac134c42de005022d3b7cb8
Rom DuPlain <rom.duplain@gmail.com>
2020-01-13 16:24:23 -0800

    Use latest Makefile.d.

--- web/.Makefile.d-init.mk
+++ web/.Makefile.d-init.mk
@@ -6,11 +6,11 @@ QWERTY_SH := $(abspath $(DIR)/../qwerty.sh)
 endif
 
 MAKEFILE_D_URL := https://github.com/rduplain/Makefile.d.git
-MAKEFILE_D_REV := 04f0ab7
+MAKEFILE_D_REV := v1.3.1
 
 .Makefile.d/%.mk: .Makefile.d/path.mk
 	@touch $@
 
 .Makefile.d/path.mk: $(MAKEFILE_D_INIT_MK)
-	$(QWERTY_SH) -f -o .Makefile.d --ref $(MAKEFILE_D_REV) $(MAKEFILE_D_URL)
+	$(QWERTY_SH) -f -o .Makefile.d --tag $(MAKEFILE_D_REV) $(MAKEFILE_D_URL)
 	@touch $@
