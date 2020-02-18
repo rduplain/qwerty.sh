38edaa50fb9e18b82c1629225fa535a38752d138
Rom DuPlain <rom.duplain@gmail.com>
2020-02-18 10:11:47 -0800

    Mark HEAD as v0.7.1.

--- qwerty.sh
+++ qwerty.sh
@@ -5,7 +5,7 @@
 # README: https://github.com/rduplain/qwerty.sh#readme
 # Contact: community@qwerty.sh -- See footer for BSD 2-Clause License.
 
-VERSION=v0.7
+VERSION=v0.7.1-dev
 
 usage() {
     exists "$@" && stderr "$PROG: $(red "$@")" && return 2
